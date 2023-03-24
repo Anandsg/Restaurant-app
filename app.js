791b7e4fdac21da91e0555d1b3c9f3b79f2941f3
Anandagadugin <gaduginanand984@gmail.com>
2023-03-24 21:17:27 +0530

    changes to crud crud api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function storeOrder() {
+async function storeOrder() {
 	try {
 		const tableNum = document.getElementById('tableNum').value;
 		const category = document.getElementById('category').value;
@@ -13,14 +13,8 @@ function storeOrder() {
 				'drink': []
 			};
 		}
-		orders[tableNum][category].push({
-			'dishName': dishName,
-			'quantity': quantity
-		});
-		localStorage.setItem('orders', JSON.stringify(orders));
-		// Make HTTP POST request to API endpoint
-		fetch('https://crudcrud.com/api/0d0ed4f85d044931abe7b50df64c928b/orders', {
-			method: 'POST',
+		const response = await fetch('https://crudcrud.com/api/cf2926fe0e734c7fba458e192eae2c56/orders', {
+			method: 'POST',	
 			headers: {
 				'Content-Type': 'application/json'
 			},
@@ -30,18 +24,27 @@ function storeOrder() {
 				'dishName': dishName,
 				'quantity': quantity
 			})
-		})
-		.then(response => response.json())
-		.then(data => console.log(data))
-		.catch(error => console.error(error));
-
+		});
+		const data = await response.json();
+		console.log(data);
+		
+		// Add the order ID to the orders object
+		orders[tableNum][category].push({
+			'dishName': dishName,
+			'quantity': quantity,
+			'_id': data._id
+		});
+		localStorage.setItem('orders', JSON.stringify(orders));
+		
 		displayOrders();
 	} catch (error) {
 		console.error(error);
 	}
 }
+
+
 // Display the orders by table
-function displayOrders() {
+async function displayOrders() {
 	try {
 		let orders = JSON.parse(localStorage.getItem('orders')) || {};
 		let ordersHTML = '';
@@ -52,7 +55,7 @@ function displayOrders() {
 					ordersHTML += '<h4>' + category.charAt(0).toUpperCase() + category.slice(1) + 's</h4>';
 					ordersHTML += '<ul>';
 					for (let i = 0; i < orders[tableNum][category].length; i++) {
-						ordersHTML += '<li>' + orders[tableNum][category][i].dishName + ' - ' + orders[tableNum][category][i].quantity + ' <button onclick="deleteOrder(' + tableNum + ', \'' + category + '\', ' + i + ')">Delete</button></li>';
+						ordersHTML += '<li>' + orders[tableNum][category][i].dishName + ' - ' + orders[tableNum][category][i].quantity + ' <button onclick="deleteOrder(' + tableNum + ', \'' + category + '\', ' + i + ', \'' + orders[tableNum][category][i]._id + '\')">Delete</button></li>';
 					}
 					ordersHTML += '</ul>';
 				}
@@ -63,23 +66,26 @@ function displayOrders() {
 		console.error(error);
 	}
 }
+
 // Delete an order
-function deleteOrder(tableNum, category, index, orderId) {
+async function deleteOrder(tableNum, category, index, orderId) {
 	try {
-		let orders = JSON.parse(localStorage.getItem('orders')) || {};
-		orders[tableNum][category].splice(index, 1);
-		localStorage.setItem('orders', JSON.stringify(orders));
-
-		// Make HTTP DELETE request to API endpoint
-		fetch('https://crudcrud.com/api/0d0ed4f85d044931abe7b50df64c928b/orders/' + orderId, {
-			method: 'DELETE'
-		})
-		.then(response => response.json())
-		.then(data => console.log(data))
-		.catch(error => console.error(error));
-
-		displayOrders();
+	  let orders = JSON.parse(localStorage.getItem('orders')) || {};
+	  orders[tableNum][category].splice(index, 1);
+	  localStorage.setItem('orders', JSON.stringify(orders));
+  
+	  // Making HTTP DELETE request to API
+	  const response = await fetch('https://crudcrud.com/api/cf2926fe0e734c7fba458e192eae2c56/orders/' + orderId, {
+		method: 'DELETE'
+	  });
+	  const data = await response.json();
+	  console.log(data);
+  
+	  displayOrders();
 	} catch (error) {
-		console.error(error);
+	  console.error(error);
 	}
-}
+  }
+  
+
+	
